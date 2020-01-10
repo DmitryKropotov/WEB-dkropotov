3fce86b208d6dca1589c737f4f1ba1f624491d5a
dkropotov <dkropotov@griddynamics.com>
2020-01-10 17:19:19 +0300

    made redirect to main page from starter page, removed unneeded imports in MainPageServlet

--- src/main/java/com/project/servlets/MainPageServlet.java
+++ src/main/java/com/project/servlets/MainPageServlet.java
@@ -1,8 +1,5 @@
 package com.project.servlets;
 
-import com.project.controllers.MainController;
-
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
